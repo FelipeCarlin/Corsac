d83b08b012ac7062e2617dc72b05ab9da4e59d40
Felipe Carlin <felipe.carlin7@gmail.com>
2022-03-24 22:43:30 -0300

    Hello World

---
+++ corsac/code/build.bat
@@ -0,0 +1,3 @@
+
+:: Compile 64-bit
+cl -nologo -Od -Z7 ..\corsac\code\win32_corsac.cpp -Fe: corsac.exe
---
+++ corsac/code/win32_corsac.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int ArgumentCount, char *ArgumentVector)
+{
+	printf("Hello world!");
+
+	return 0;
+}
